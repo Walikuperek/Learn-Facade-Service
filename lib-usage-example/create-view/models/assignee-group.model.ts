d98a4d757b67c9f1a529e4df337f486043691398
Walikuperek <56729104+Walikuperek@users.noreply.github.com>
2023-01-23 01:34:56 +0100

    Update assignee-group.model.ts

--- lib-usage-example/create-view/models/assignee-group.model.ts
+++ lib-usage-example/create-view/models/assignee-group.model.ts
@@ -2,10 +2,12 @@ import {Assignee} from "./assignee.model";
 import {GroupType} from "../enums";
 
 export class AssigneeGroup {
-    id: string;
-    name: string;
-    type: GroupType;
-    assignees: Assignee[];
+    id: string = '';
+    name: string = '';
+    type: GroupType | null = null;
+    assignees: Assignee[] = [];
 
-    constructor(...
+    constructor(initialData: Partial<AssigneeGroup>) {
+        Object.assign(this, initialData);
+    }
 }
